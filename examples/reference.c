5b8a5804dbf7d688edd989abc5193aaed295fa7c
andrew-aladev <aladjev.andrew@gmail.com>
2013-12-18 01:04:59 +0300

    library is not thread safe, fixed example

--- examples/reference.c
+++ examples/reference.c
@@ -10,88 +10,45 @@
 #include <tralloc/events.h>
 #endif
 
-#include <pthread.h>
-#include <unistd.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-static inline
-void random_sleep ()
+uint8_t code_1 ( char * common )
 {
-    uint miliseconds = ( ( double ) rand() ) / RAND_MAX * 300 + 100; // from 100 to 300 miliseconds
-    usleep ( miliseconds * 1000 );
-}
-
-void * thread_1 ( void * data )
-{
-    char * common           = data;
     void * common_reference = tralloc_add_reference ( common, NULL );
     if ( common_reference == NULL ) {
-        return ( void * ) 1;
+        return 1;
     }
-    random_sleep();
-    tralloc_free ( common_reference ); // may free common
-    return ( void * ) 0;
+    return tralloc_free ( common_reference ); // may free common
 }
 
-void * thread_2 ( void * data )
+uint8_t code_2 ( char * common )
 {
-    char * common           = data;
     void * common_reference = tralloc_add_reference ( common, NULL );
     if ( common_reference == NULL ) {
-        return ( void * ) 1;
+        return 1;
     }
-    random_sleep();
-    tralloc_free ( common_reference ); // may free common
-    return ( void * ) 0;
+    return tralloc_free ( common_reference ); // may free common
 }
 
 int main ()
 {
-    tralloc_context * root = tralloc_new ( NULL );
-    if ( root == NULL ) {
+    char * common = tralloc_strdup ( NULL,
+                                     "This is big data, that you dont want to duplicate."
+                                     "Different code needs it."
+                                     "You want it to be fried automaticaly when all code have finished it's processing."
+                                   );
+    if ( common == NULL ) {
         return 1;
     }
-    char * common = tralloc_strdup (
-        NULL,
-        "This is big data, that you dont want to duplicate."
-        "Different code needs it."
-        "You want it to be fried automaticaly when all code have finished it's processing."
-    );
-    if ( common == NULL ) {
-        tralloc_free ( root );
-        return 2;
-    }
-
-    time_t seed = time ( NULL );
-    srand ( seed );
-
-    pthread_t thread_1_id, thread_2_id;
     if (
-        pthread_create ( &thread_1_id, NULL, thread_1, common ) != 0 ||
-        pthread_create ( &thread_2_id, NULL, thread_2, common ) != 0
+        code_1 ( common ) != 0 ||
+        code_2 ( common ) != 0
     ) {
-        tralloc_free ( root );
+        tralloc_free ( common );
         return 2;
-    }
-
-    void * result;
-    if (
-        pthread_join ( thread_1_id, &result ) != 0 || ( uint8_t ) result != 0 ||
-        pthread_join ( thread_2_id, &result ) != 0 || ( uint8_t ) result != 0
-    ) {
-        tralloc_free ( root );
-        return 3;
-    }
-
-    if ( tralloc_free ( root ) != 0 ) {
-        return 4;
     }
 
 #if defined(TRALLOC_DEBUG)
     if ( tralloc_get_chunks_count() != 0 ) {
-        return 5;
+        return 3;
     }
 #endif
 
