67544d19238c33db930365af630d2cdb6a180d8d
Andrew Aladjev <aladjev.andrew@gmail.com>
2014-06-06 16:17:43 +0300

    cosmetics

--- test-all.sh
+++ test-all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-CC="gcc-3.4.6" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
-CC="gcc-4.8.2" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
-CC="tcc" ../combinations.sh -DTRALLOC_SHARED=0 -DCMAKE_BUILD_TYPE="RELEASE"
-NO_TESTS=1 ../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/arduino.cmake -DTRALLOC_SHARED=0 -DCMAKE_BUILD_TYPE="RELEASE"
-CC="clang" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
-CC="ccc-analyzer" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="gcc-4.8.2" CFLAGS="-Werror" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="gcc-4.9.0" CFLAGS="-Werror" ../combinations.sh -DTRALLOC_HAVE_LTO=0 -DCMAKE_BUILD_TYPE="RELEASE"
+NO_TESTS=1 CFLAGS="-Werror" ../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/arduino.cmake -DTRALLOC_SHARED=0 -DCMAKE_BUILD_TYPE="RELEASE"
+CC="tcc" CFLAGS="-Werror" ../combinations.sh -DTRALLOC_SHARED=0 -DCMAKE_BUILD_TYPE="RELEASE"
+CC="clang" CFLAGS="-Werror" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="ccc-analyzer" CFLAGS="-Werror" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="clang" CFLAGS="-Werror -fsanitize=leak -fno-omit-frame-pointer" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="clang" CFLAGS="-Werror -fsanitize=address -fno-omit-frame-pointer" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
+CC="clang" CFLAGS="-Werror -fsanitize=thread -fno-omit-frame-pointer" ../combinations.sh -DCMAKE_BUILD_TYPE="RELEASE"
