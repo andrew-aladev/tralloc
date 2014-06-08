610c6474838c0df1a042a816a679496ed2665da7
andrew-aladev <aladjev.andrew@gmail.com>
2014-06-09 01:29:18 +0300

    cosmetics

--- test-all.sh
+++ test-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-3.4.6.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-4.7.3.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-4.8.2.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/tcc.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-3.4.6.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-4.7.3.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/gcc-4.8.2.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/tcc.cmake       -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang.cmake     -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
 
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/ccc-analyzer.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-leak.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-address.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
-../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-thread.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/ccc-analyzer.cmake  -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-leak.cmake    -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-address.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
+../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-thread.cmake  -DCMAKE_BUILD_TYPE="RELEASE" && rm -r * &&
 
 NO_TESTS=1 ../combinations.sh -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/avr-gcc-4.8.2.cmake -DCMAKE_BUILD_TYPE="RELEASE" && rm -r *
