e09ea64e31a991f6171c90e7f5076d06524cc694
Andrew Aladjev <aladjev.andrew@gmail.com>
2014-11-04 18:54:42 +0300

    cosmetics

--- cmake/toolchains/gcc/3.3/default.cmake
+++ cmake/toolchains/gcc/3.3/default.cmake
@@ -1,3 +1,3 @@
 set (CMAKE_SYSTEM_NAME "Linux")
 set (CMAKE_C_COMPILER "gcc-3.3.6")
-set (CMAKE_C_FLAGS "-fno-strict-aliasing" CACHE STRING "gcc-3.3.6 cflags")
+set (CMAKE_C_FLAGS "-Werror -fno-strict-aliasing" CACHE STRING "gcc-3.3.6 cflags")
--- cmake/toolchains/gcc/4.9/avr.cmake
+++ cmake/toolchains/gcc/4.9/avr.cmake
@@ -1,3 +1,3 @@
 set (CMAKE_SYSTEM_NAME "Generic")
-set (CMAKE_C_COMPILER "avr-gcc-4.9.1")
-set (CMAKE_C_FLAGS "-Werror -Wno-maybe-uninitialized -mmcu=atmega328 -DF_CPU=16000000UL -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums" CACHE STRING "avr-gcc-4.9.1 cflags")
+set (CMAKE_C_COMPILER "avr-gcc-4.9.2")
+set (CMAKE_C_FLAGS "-Werror -Wno-maybe-uninitialized -mmcu=atmega328 -DF_CPU=16000000UL -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums" CACHE STRING "avr-gcc-4.9.2 cflags")
