4c8e17bee327cb66d7c33ecce9a29195b6683bb8
andrew-aladev <aladjev.andrew@gmail.com>
2014-11-05 00:58:16 +0300

    cosmetics

--- cmake/toolchains/gcc/4.0/default.cmake
+++ cmake/toolchains/gcc/4.0/default.cmake
@@ -1,3 +1,3 @@
 set (CMAKE_SYSTEM_NAME "Linux")
 set (CMAKE_C_COMPILER "gcc-4.0.4")
-set (CMAKE_C_FLAGS "-Werror -Wno-maybe-uninitialized" CACHE STRING "gcc-4.0.4 cflags")
+set (CMAKE_C_FLAGS "-fno-strict-aliasing" CACHE STRING "gcc-4.0.4 cflags")
