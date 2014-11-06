c3c75e0373def2753cae8207952499419c4365f4
Andrew Aladjev <admin@puchuu.com>
2014-11-06 11:22:34 +0000

    cosmetics

--- cmake/scripts/test-all.sh
+++ cmake/scripts/test-all.sh
@@ -7,6 +7,8 @@ for version in 3.3 3.4 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9; do
     commands+=("NO_TESTS=1 $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/gcc/$version/default.cmake\" -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1")
 done
 
+commands+=("NO_TESTS=1 $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/clang/default.cmake\" -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1")
+
 function join {
     local IFS="$1";
     shift;
@@ -19,7 +21,6 @@ eval "nice -n 19 ionice -c2 -n7 sh -c \"$commands\""
 
 # $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/tcc/default.cmake\" -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1 &&
 # 
-# $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/clang/default.cmake\"           -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1 &&
 # $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/clang/ccc-analyzer.cmake\"      -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1 &&
 # $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/clang/leak-sanitizer.cmake\"    -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1 &&
 # $current_dir/combinations.sh -DCMAKE_TOOLCHAIN_FILE=\"$current_dir/../toolchains/clang/address-sanitizer.cmake\" -DCMAKE_BUILD_TYPE=\"RELEASE\" -DCMAKE_CONFIG_VERBOSE_MAKEFILE=1 -DCMAKE_VERBOSE_MAKEFILE=1 &&
