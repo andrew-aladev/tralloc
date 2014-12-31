95616ab5cda7cc928c95094f8a3fef942cff5687
andrew-aladev <aladjev.andrew@gmail.com>
2015-01-01 02:57:07 +0300

    cosmetics

--- cmake/finds/FindAsciidoc.cmake
+++ cmake/finds/FindAsciidoc.cmake
@@ -1,13 +1,13 @@
-if (DEFINED ASCIIDOC_A2X_EXECUTABLE)
+if (DEFINED TRALLOC_ASCIIDOC_A2X_EXECUTABLE)
     return ()
 endif ()
 
-find_program (ASCIIDOC_A2X_EXECUTABLE "a2x")
+find_program (TRALLOC_ASCIIDOC_A2X_EXECUTABLE "a2x")
 
-if (ASCIIDOC_A2X_EXECUTABLE)
-    set (ASCIIDOC_A2X_FOUND true CACHE STRING "Status of asciidoc")
+if (TRALLOC_ASCIIDOC_A2X_EXECUTABLE)
+    set (TRALLOC_ASCIIDOC_A2X_FOUND true CACHE STRING "Status of asciidoc")
     message (STATUS "Look for \"asciidoc\" - yes")
 else ()
-    set (ASCIIDOC_A2X_FOUND false CACHE STRING "Status of asciidoc")
+    set (TRALLOC_ASCIIDOC_A2X_FOUND false CACHE STRING "Status of asciidoc")
     message (STATUS "Look for \"asciidoc\" - no")
 endif ()
